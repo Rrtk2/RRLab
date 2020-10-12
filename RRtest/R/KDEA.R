c27ece03033bfd9df968839b8c15073f40c0f2cc
Rick <Rick.Reijnders@home.nl>
2020-10-12 12:34:17 +0200

    Added parallel computing on intesive part in KDEA

--- RRtest/R/KDEA.R
+++ RRtest/R/KDEA.R
@@ -27,18 +27,19 @@
 #' @param s_logFCTH Minimum logFC required threshold (float).
 #' @param s_CovFormula Defaults to "~Class"; can be changed, however, the column entered in f_dataset_class_column_id is called class, and in formula should be treated so. Example: in iris the function can be run using KDEA(iris,5). The fifth column ("species") will be renamed to "Class" therefore the formula should be "~Class" and not "~Species". Other columns keep their names, therefore "~Class+Sepal.Width" is possible. This function extracts the 1st term from the formula, this is set by s_CovOfImportance, which can be changed appropriately. 
 #' @param s_CovOfImportance Automatically searches the correct "Class" column, usally 1; This is the column which is extracted after running limma::topTable. When s_CovFormula = NA then the first column is the LogFC column. When edited to eg "~Class+Sepal.Width", the first column is the "Class" FC and the second column is the effect of "Sepal.Width". Make sure this matches.
-#' @param s_ShowPlot Create plots (can take a long time when evaluating big datasets (50k+ features) set to TRUE (default)
+#' @param s_MakePlot Create plots (can take a long time when evaluating big datasets (50k+ features) set to TRUE (default)
 #' @param s_Verbose Show more info if TRUE (default)
 #'
 #' @return A list of imporant features, a dataframe of ranked features, the plot, and indermediate results.
 #' @importFrom magrittr "%>%"
+#' @importFrom foreach `%dopar%`
 #' @examples
 #' KDEA(dataset = dataset, s_PhenoDataFrame = Phenotipic_data, s_CovFormula = "~Class + age + sex")
 #' KDEA(dataset = mtcars, f_dataset_class_column_id = which(colnames(mtcars)=="am"), s_CovFormula = "~Class + gear")
 #' KDEA(dataset = iris, f_dataset_class_column_id = 5, s_CovOfImportance = 1)
 #' @export
 
-KDEA = function(dataset = NA, f_dataset_class_column_id = NA, s_PhenoDataFrame = NA,s_omitNA = TRUE, s_partitionlength = 0.5, s_k = floor(sqrt(dim(dataset)[1])), s_pvalTH = 0.05, s_AmountSignTH = floor(s_k*0.3), s_logFCTH=NA, s_CovFormula = NA, s_CovOfImportance = NA, s_ShowPlot=TRUE s_Verbose=TRUE) {
+KDEA = function(dataset = NA, f_dataset_class_column_id = NA, s_PhenoDataFrame = NA,s_omitNA = TRUE, s_partitionlength = 0.5, s_k = floor(sqrt(dim(dataset)[1])), s_pvalTH = 0.05, s_AmountSignTH = floor(s_k*0.3), s_logFCTH=NA, s_CovFormula = NA, s_CovOfImportance = NA, s_MakePlot=TRUE, s_Verbose=TRUE) {
 
 #-----------------------------------------------------------------------------------------------------#
 #							checks
@@ -47,11 +48,17 @@ KDEA = function(dataset = NA, f_dataset_class_column_id = NA, s_PhenoDataFrame =
 # if(is.na(dataset)){stop("Please enter dataset")}
 
 # it cannot be more significant (number of times) than the amount of folds
-s_AmountSignTH = min(s_AmountSignTH,s_k)-1
+s_AmountSignTH = min(s_AmountSignTH,s_k)
+
+
 
 # check if phenotypic dataframe is added
 if(!is.null(dim(s_PhenoDataFrame)[1])){
-	cat(paste0("Using phenotypic dataframe for Class and covariates (if needed):\nMake sure rownames(dataset) and rownames(s_PhenoDataFrame) use the same identifiers!!"))
+
+	# make sure s_PhenoDataFrame is dataframe
+	s_PhenoDataFrame = as.data.frame(s_PhenoDataFrame)
+
+	cat(paste0("Using phenotypic dataframe for Class and covariates (if needed):\nMake sure rownames(dataset) and rownames(s_PhenoDataFrame) use the same identifiers!!\n"))
 	
 	# test if dataframe
 	if(!is.data.frame(s_PhenoDataFrame)){
@@ -72,15 +79,21 @@ if(!is.null(dim(s_PhenoDataFrame)[1])){
 	# get the linking column between dataset and pheno
 	IdNameColInPhenoDF = names(which(lapply(s_PhenoDataFrame,FUN = function(X){sum(as.character(rownames(dataset))%in%as.character(X))})>0)[1])
 	
-	# Select data based on phenotype order.
-	relevantPhenoDFOrder = as.numeric(match(as.character(rownames(dataset)),x=as.character(s_PhenoDataFrame[[IdNameColInPhenoDF]])))
-	dataset = dataset[relevantPhenoDFOrder,]
+	# print found linker
+	cat(paste0("Found column '",IdNameColInPhenoDF,"' as universal linker between pheno and data\n"))
 	
+	# Add the linker to rownames of frame, this is required for temp_design
+	rownames(s_PhenoDataFrame) = as.character(s_PhenoDataFrame[[IdNameColInPhenoDF]])
+
+	# Select class data based on phenotype PATNO
 	if(sum(colnames(dataset)=="Class")==1){
-		dataset$Class = s_PhenoDataFrame$Class
+		dataset$Class = s_PhenoDataFrame$Class[match(x=rownames(dataset),s_PhenoDataFrame[[IdNameColInPhenoDF]])]
 		}else{
-		dataset = data.frame(dataset, Class = s_PhenoDataFrame$Class)
+		dataset = data.frame(dataset, Class = s_PhenoDataFrame$Class[match(x=rownames(dataset),s_PhenoDataFrame[[IdNameColInPhenoDF]])])
 	}
+	
+	# match s_PhenoDataFrame to dataset; as s_PhenoDataFrame is bigger and thus has different positions for Trainindex
+	s_PhenoDataFrame = s_PhenoDataFrame[match(rownames(dataset),rownames(s_PhenoDataFrame)),]
 
 }
 
@@ -162,15 +175,11 @@ for( i in 1:s_k){
 	# Make contrasts based on "~Class" or used defined
 	# if a pheno DF os given:
 	if(!is.null(dim(s_PhenoDataFrame)[1])){
-		dataset
-	
-		s_PhenoDataFrame
-	
+		
 		temp_design <- model.matrix(as.formula(s_CovFormula), data = s_PhenoDataFrame[Trainindex,])
-		
-		
-		
+
 		}else{
+		
 		temp_design <- model.matrix(as.formula(s_CovFormula), data = dataset[Trainindex,])
 	}
 
@@ -225,16 +234,45 @@ for(i in 2:s_k){
 df$Pval = -log10(df$Pval)
 df$Pval = round(df$Pval,4)
 
-# Create secific data for result (medians of FC and Pval)
-RankedOrderedData = data.frame(FeatureName=unique(df$names),MedianLogFC=NA,MedianLog10Pval=NA)
-for(i in 1:length(unique(df$names))){
-	tempIndex = df$names==unique(df$names)[i]
-	tempdf = df[tempIndex,]
+## Create secific data for result (medians of FC and Pval)
+#RankedOrderedData = #data.frame(FeatureName=unique(df$names),MedianLogFC=NA,MedianLog10Pval=NA)
+#for(i in 1:length(unique(df$names))){
+#	tempIndex = df$names==unique(df$names)[i]
+#	tempdf = df[tempIndex,]
+#	
+#	#rownames(DF)[i] = unique(df$names)[i]
+#	RankedOrderedData[i,"MedianLogFC"] = median(tempdf$FC)
+#	RankedOrderedData[i,"MedianLog10Pval"] = median(tempdf$Pval)
+#	
+#	if(i==floor(length(unique(df$names))*0.10)){cat("10% done\n")}
+#	if(i==floor(length(unique(df$names))*0.25)){cat("25% done\n")}
+#	if(i==floor(length(unique(df$names))*0.50)){cat("50% done\n")}
+#	if(i==floor(length(unique(df$names))*0.75)){cat("75% done\n")}
+#}
+
+#setup parallel backend to use many processors
+cores=doParallel::detectCores()
+cl <- doParallel::makeCluster(cores[1]-1) #not to overload your computer
+doParallel::registerDoParallel(cl)
+
+RankedOrderedData <- foreach::foreach(i=1:length(unique(df$names)), .combine=rbind) %dopar% {
+  
+	Featurename = unique(df$names)[i]
+  
+	temp_1 = df[df$names==Featurename,]
+    
+	MedianLogFC = median(temp_1$FC)
 	
-	#rownames(DF)[i] = unique(df$names)[i]
-	RankedOrderedData[i,"MedianLogFC"] = median(tempdf$FC)
-	RankedOrderedData[i,"MedianLog10Pval"] = median(tempdf$Pval)
+	MedianLog10Pval = median(temp_1$Pval)
+  
+	tempMatrix = data.frame(Featurename = Featurename, MedianLogFC = MedianLogFC,MedianLog10Pval=MedianLog10Pval)
+	
+	#tempMatrix = c(Featurename,MedianLogFC,MedianLog10Pval)
+
+   tempMatrix #Equivalent to finalMatrix = rbind(finalMatrix, tempMatrix)
 }
+#stop cluster
+doParallel::stopCluster(cl)
 
 
 
@@ -285,84 +323,7 @@ RankedOrderedData = RankedOrderedData[order(RankedOrderedData$RankCombined),]
 	
 	#### CHECK ####
 	if(dim(df_f)[1]==0){
-		warning("No significant values to present, increase s_pvalTH")
-		out=list(Rankobject = RankedOrderedData, res_super = res_super)
-		return(out)
-	}else{
-		
-		# present the results in table
-		Plot = df_f  %>%
-		dplyr::mutate(names = fct_reorder(names, abs(FC))) %>%
-		ggplot2::ggplot(
-			aes(x = names, y = FC, fill = Pval)
-		)+
-		ggplot2::scale_y_continuous(
-			breaks = round(seq(-ceiling(max(abs(df_f$FC))),ceiling(max(abs(df_f$FC))),(ceiling(max(abs(df_f$FC)))/3)),2),
-			limits = c(-ceiling(max(abs(df_f$FC))*10)/10, ceiling(max(abs(df_f$FC))*10)/10), expand =  c(0.01, 0.01)
-		)+
-		ggplot2::ggtitle(paste0("Robust features using ",names(temp_results)[s_CovOfImportance],"\n",s_CovFormula,"\n")
-		)+
-		#scale_color_manual(
-		#	name="-log10(P-value)",
-		#)+
-		#scale_colour_discrete(
-		#name = '-log10(P-value)', values = heat.colors(15),discrete = TRUE
-		#)+
-		ggplot2::scale_fill_gradientn(
-		name="Significance\n-log10(P-value)",
-			colours		= c("gray70","gray70","yellow","red"),
-			#space		= "Lab",
-			 #high		= "blue",
-			 #low		= "yellow",
-			 #limits 	= c(-log10(0.05), 5),
-			 na.value	= "gray50",
-			 values   	= c(0,((-log10(0.05)-0.000000001)-min(df_f$Pval))/(max(df_f$Pval)-min(df_f$Pval)),(-log10(0.05)-min(df_f$Pval))/(max(df_f$Pval)-min(df_f$Pval)),1),#,values   	= c(0,-log10(0.05)/5,5/5)#, (min(df_f$Pval)-min(df_f$Pval))/(max(df_f$Pval)-min(df_f$Pval))
-			 breaks   	= round(c(-log10(0.05),2,3,4,max(df_f$Pval)),1)
-		)+
-		ggplot2::guides(
-			fill = guide_colourbar(barwidth = 10, barheight = 1,ticks.colour = "black", ticks.linewidth = 0.8)
-		)+
-		ggplot2::geom_hline(
-			yintercept = 0, colour="gray50",linetype = "longdash", alpha = 0.60#linetype = "shortdash",
-		)+
-		ggplot2::geom_jitter(
-			size = 4, alpha = (df_f$Pval-min(df_f$Pval))/(max(df_f$Pval)-min(df_f$Pval))+0.2, width = 0.0, shape = 21
-		)+
-		ggplot2::stat_summary(
-			fun = median, geom = "point", size = 7,colour = "black",alpha = 1, aes(shape="Median")
-		#)+
-		#	scale_shape_manual(
-		#		"", values=c("Median"="I")
-		)+
-		ggplot2::stat_summary(
-			fun = mean, geom = "point", size = 3,colour = "black",alpha = 1, aes(shape="Mean")
-		)+
-		ggplot2::scale_shape_manual(
-				"", values=c("Mean"="bullet","Median"="I")
-		)+
-		#geom_hline(
-		#	yintercept = c(-0.5), colour="gray50",alpha = c(0.4)#linetype = "shortdash",
-		#)+
-		#geom_hline(
-		#	yintercept = c(0.5), colour="gray50",alpha = c(0.4)#linetype = "shortdash",
-		#)+		
-		ggplot2::labs(
-			caption = NULL, x = "Feature labels", y = "Fold change (log2)"
-		)+ggplot2::theme(
-			# Font sizes
-			strip.text.x = element_text(size = 12),
-			text = element_text(size=14),
-			legend.text=element_text(size = 12),
-			legend.position="bottom"
-		)+
-		ggplot2::coord_flip() 
-		
-		res_plotfeatures = unique((df_f[,1]))
-		
-		
-		#-----------------------------------------------------------------------------------------------------#
-		#							Get all metrics (settings)
-		#-----------------------------------------------------------------------------------------------------#
+	
 		# gather all settings and dump in var for reproducebility
 		allSettings  = ls(pattern="^s_")
 		s_settings = NA
@@ -377,9 +338,123 @@ RankedOrderedData = RankedOrderedData[order(RankedOrderedData$RankCombined),]
 		}
 		s_settings = temp
 		rm(temp)
-		
-		out=list(Rankobject = RankedOrderedData, Plot=Plot, PlotFeatues = res_plotfeatures, res_super = res_super, Rawdata = df, formula = s_CovFormula, settings = s_settings)
-		if(s_ShowPlot){print(Plot)}
+	
+		warning("No significant values to present, increase s_pvalTH")
+		out=list(Rankobject = RankedOrderedData, Plot=NA, PlotFeatues = NA, res_super = res_super, Rawdata = df, formula = s_CovFormula, design = temp_design, settings = s_settings)
+		return(out)
+	}else{
+		# check if plot needs to be made
+		if(s_MakePlot){
+			# present the results in table
+			Plot = df_f  %>%
+			dplyr::mutate(names = fct_reorder(names, abs(FC))) %>%
+			ggplot2::ggplot(
+				aes(x = names, y = FC, fill = Pval)
+			)+
+			ggplot2::scale_y_continuous(
+				breaks = round(seq(-ceiling(max(abs(df_f$FC))),ceiling(max(abs(df_f$FC))),(ceiling(max(abs(df_f$FC)))/3)),2),
+				limits = c(-ceiling(max(abs(df_f$FC))*10)/10, ceiling(max(abs(df_f$FC))*10)/10), expand =  c(0.01, 0.01)
+			)+
+			ggplot2::ggtitle(paste0("Robust features using ",names(temp_results)[s_CovOfImportance],"\n",s_CovFormula,"\n")
+			)+
+			#scale_color_manual(
+			#	name="-log10(P-value)",
+			#)+
+			#scale_colour_discrete(
+			#name = '-log10(P-value)', values = heat.colors(15),discrete = TRUE
+			#)+
+			ggplot2::scale_fill_gradientn(
+			name="Significance\n-log10(P-value)",
+				colours		= c("gray70","gray70","yellow","red"),
+				#space		= "Lab",
+				 #high		= "blue",
+				 #low		= "yellow",
+				 #limits 	= c(-log10(0.05), 5),
+				 na.value	= "gray50",
+				 values   	= c(0,((-log10(0.05)-0.000000001)-min(df_f$Pval))/(max(df_f$Pval)-min(df_f$Pval)),(-log10(0.05)-min(df_f$Pval))/(max(df_f$Pval)-min(df_f$Pval)),1),#,values   	= c(0,-log10(0.05)/5,5/5)#, (min(df_f$Pval)-min(df_f$Pval))/(max(df_f$Pval)-min(df_f$Pval))
+				 breaks   	= round(c(-log10(0.05),2,3,4,max(df_f$Pval)),1)
+			)+
+			ggplot2::guides(
+				fill = guide_colourbar(barwidth = 10, barheight = 1,ticks.colour = "black", ticks.linewidth = 0.8)
+			)+
+			ggplot2::geom_hline(
+				yintercept = 0, colour="gray50",linetype = "longdash", alpha = 0.60#linetype = "shortdash",
+			)+
+			ggplot2::geom_jitter(
+				size = 4, alpha = (df_f$Pval-min(df_f$Pval))/(max(df_f$Pval)-min(df_f$Pval))+0.2, width = 0.0, shape = 21
+			)+
+			ggplot2::stat_summary(
+				fun = median, geom = "point", size = 7,colour = "black",alpha = 1, aes(shape="Median")
+			#)+
+			#	scale_shape_manual(
+			#		"", values=c("Median"="I")
+			)+
+			ggplot2::stat_summary(
+				fun = mean, geom = "point", size = 3,colour = "black",alpha = 1, aes(shape="Mean")
+			)+
+			ggplot2::scale_shape_manual(
+					"", values=c("Mean"="bullet","Median"="I")
+			)+
+			#geom_hline(
+			#	yintercept = c(-0.5), colour="gray50",alpha = c(0.4)#linetype = "shortdash",
+			#)+
+			#geom_hline(
+			#	yintercept = c(0.5), colour="gray50",alpha = c(0.4)#linetype = "shortdash",
+			#)+		
+			ggplot2::labs(
+				caption = NULL, x = "Feature labels", y = "Fold change (log2)"
+			)+ggplot2::theme(
+				# Font sizes
+				strip.text.x = element_text(size = 12),
+				text = element_text(size=14),
+				legend.text=element_text(size = 12),
+				legend.position="bottom"
+			)+
+			ggplot2::coord_flip() 
+			
+			res_plotfeatures = unique((df_f[,1]))
+			
+			
+			# gather all settings and dump in var for reproducebility
+			allSettings  = ls(pattern="^s_")
+			s_settings = NA
+			temp = data.frame(Setting = allSettings, Value=NA)
+			for(o in 1:length(allSettings)){
+				if(allSettings[o]=="s_PhenoDataFrame"){next} # skip for now @RRR
+				tempset = allSettings[o]
+				tempval = NA
+				tempval = get(tempset)
+				#temp[o,1] = tempset
+				temp[o,2] = tempval
+			}
+			s_settings = temp
+			rm(temp)
+			
+			
+			print(Plot)
+			out=list(Rankobject = RankedOrderedData, Plot=Plot, PlotFeatues = res_plotfeatures, res_super = res_super, Rawdata = df, formula = s_CovFormula, design = temp_design, settings = s_settings)
+			
+			
+		}else{
+			# if no plot is made then do:
+			
+			# gather all settings and dump in var for reproducebility
+			allSettings  = ls(pattern="^s_")
+			s_settings = NA
+			temp = data.frame(Setting = allSettings, Value=NA)
+			for(o in 1:length(allSettings)){
+				if(allSettings[o]=="s_PhenoDataFrame"){next} # skip for now @RRR
+				tempset = allSettings[o]
+				tempval = NA
+				tempval = get(tempset)
+				#temp[o,1] = tempset
+				temp[o,2] = tempval
+			}
+			s_settings = temp
+			rm(temp)
+			
+			out=list(Rankobject = RankedOrderedData, Plot=NA, PlotFeatues = NA, res_super = res_super, Rawdata = df, formula = s_CovFormula, design = temp_design, settings = s_settings)
+		}
 		return(out)
 	}
 		
--- RRtest/R/zzz.R
+++ RRtest/R/zzz.R
@@ -4,7 +4,7 @@
 	packageStartupMessage(cat("Welcome to the RRtest package! \nPlease help me improve at GitHub(RRtk2/RRtest)!\nPlease run devtools::install_github('RRtk2/RRtest/RRtest') to get the latest version.\n"))
 	
 	# Set packages
-	s_requiredpackages = c("ggplot2","ggfortify","matlib","fitdistrplus","caret","limma","tidyverse","ggsci","showtext") #,"parallel"
+	s_requiredpackages = c("ggplot2","ggfortify","matlib","fitdistrplus","caret","limma","tidyverse","ggsci","showtext","foreach","doParallel") #,"parallel"
 
 	# Install packages if needed and load, or just load packages
 	if (!requireNamespace("BiocManager", quietly = TRUE))
