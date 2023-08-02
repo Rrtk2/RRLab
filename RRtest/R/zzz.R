472a2492da28a144073cb1c3f7bd8f8352bb5317
Rick <Rick.Reijnders@home.nl>
2023-08-02 10:50:26 +0200

    Bugfix datatable not finding function "."

--- RRtest/R/zzz.R
+++ RRtest/R/zzz.R
@@ -4,7 +4,7 @@
 	packageStartupMessage(cat("Welcome to the RRtest package! \nPlease help me improve at GitHub(RRtk2/RRtest)!\nPlease run devtools::install_github('RRtk2/RRtest/RRtest') to get the latest version.\n"))
 	
 	# Set packages
-	s_requiredpackages = c("plyr", "dplyr","ggplot2","ggfortify","matlib","fitdistrplus","caret","limma","tidyverse","ggsci","showtext","foreach","doParallel","parallel","progress","data.table","splitstackshape") #,"parallel"
+	s_requiredpackages = c("ggplot2","ggfortify","matlib","fitdistrplus","caret","limma","tidyverse","ggsci","showtext","foreach","doParallel","parallel","progress","data.table","splitstackshape") #,"parallel"
 
 	# Install packages if needed and load, or just load packages
 	if (!requireNamespace("BiocManager", quietly = TRUE))
@@ -13,7 +13,7 @@
 	for (i in s_requiredpackages) {
 		if (!requireNamespace(i, quietly = TRUE))
 			BiocManager::install(i, ask = F)  # dependencies = c("Depends", "Imports")
-		require(as.character(i), character.only = TRUE)
+		library(as.character(i), character.only = TRUE)
 		#print(i)
 	}
 
