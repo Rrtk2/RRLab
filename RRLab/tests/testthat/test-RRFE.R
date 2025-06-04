f0ef87d5fb7ed2234e093745f0cf5e26382c5893
Rick <17815109+Rrtk2@users.noreply.github.com>
2025-06-04 11:22:47 +0200

    Remove testthat tests

--- RRLab/tests/testthat/test-RRFE.R
+++
@@ -1,9 +0,0 @@
-library(testthat)
-
-test_that("RRFE runs on iris subset", {
-  skip_on_cran()
-  data <- iris[1:20, ]
-  result <- RRFE(dataset = data, f_dataset_class_column_id = 5,
-                 s_MaxComponents = 2, s_KRepeats = 1,
-                 s_KmeansRepeat = 1, ShowPlots = FALSE, verbose = FALSE)
-  expect_type(result, "list")
